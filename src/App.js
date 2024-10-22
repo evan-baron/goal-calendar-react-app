7486e851fcf40ff77d01ecc85b91891545888882
evan baron <egbaron@gmail.com>
2024-10-22 15:52:26 -0600

    testing around with MUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import './App.css';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import { createTheme } from '@mui/material/styles';
+import { blue } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: blue[300],
+      main: blue[500],
+      dark: blue[700],
+      darker: blue[900],
+    },
+  },
+});
 
 function App() {
   return (
-    <>
-      <h1>Hello world!</h1>
-    </>
+    <div className="App">
+      <Button 
+        variant="contained"
+        onClick={() => console.log("user clicked")}
+        >First MUI Component</Button>
+      <Button 
+        variant="contained"
+        onClick={() => console.log("user clicked")}
+        sx={{ bgcolor: 'primary.light'}}
+        >Second MUI Component</Button>
+    </div>
   );
 }
 
