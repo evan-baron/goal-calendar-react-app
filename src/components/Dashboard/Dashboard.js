0bd6cb2e28877e4e1718bfd10076aa7be9ab0b53
evan baron <egbaron@gmail.com>
2024-11-19 20:54:10 -0700

    foundation of dashboard built

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,8 +3,15 @@ import './Dashboard.css'
 
 const Dashboard = () => {
   return (
-    <div className='dashboard'>
-        
+    <div className='dashboard-container'>
+        <div className='calendar-container'>
+            <div className='calendar-name'>Calendar Name</div>
+            <div className='dashboard-calendar'>Calendar</div>
+        </div>
+        <div className='dashboard-calendar-support'>
+            <div className='support-container dashboard-tasks'>Dashboard Tasks</div>
+            <div className='support-container dashboard-stats'>Dashboard Stats</div>
+        </div>
     </div>
   )
 }
