b242fa7ccae6ca8a9237b68883325959d1188b15
evan baron <egbaron@gmail.com>
2024-11-26 17:21:49 -0700

    changed footer css a bit

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -1,42 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Footer.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="footer-nav">
+    <div className='footer'>
+      <div className='footer-nav'>
         <section>
-          <div className="footer-logo-title">
-            <Link to="/">marbl'r</Link>
+          <div className='footer-logo-title'>
+            <Link to='/'>marbl'r</Link>
           </div>
         </section>
         <section>
-          <ul className="footer-nav-section">
-            <li className="footer-nav-link footer-nav-title">Company</li>
-            <li className="footer-nav-link">
-              <Link to="/about">About</Link>
+          <ul className='footer-nav-section'>
+            <li className='footer-nav-link footer-nav-title'>Company</li>
+            <li className='footer-nav-link'>
+              <Link to='/about'>About</Link>
             </li>
-            <li className="footer-nav-link">
-              <Link to="/privacy">Privacy Policy</Link>
+            <li className='footer-nav-link'>
+              <Link to='/privacy'>Privacy Policy</Link>
             </li>
           </ul>
         </section>
         <section>
-          <ul className="footer-nav-section">
-            <li className="footer-nav-link footer-nav-title">Help</li>
-            <li className="footer-nav-link">
-              <Link to="/contact">Contact</Link>
+          <ul className='footer-nav-section'>
+            <li className='footer-nav-link footer-nav-title'>Help</li>
+            <li className='footer-nav-link'>
+              <Link to='/contact'>Contact</Link>
             </li>
-            <li className="footer-nav-link">
-              <Link to="/account">Account Info</Link> {/* only make visible if logged in */}
+            <li className='footer-nav-link'>
+              <Link to='/account'>Account Info</Link>{' '}
+              {/* only make visible if logged in */}
             </li>
           </ul>
         </section>
       </div>
-      <div className="copyright">copyright text goes here</div>
+      <div className='copyright'>copyright text goes here</div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
